86ffffa2fcb64d0255261c3e987cae19e9fa07b2
Mohammad Bayani <mohammadmahdisharafbayani@gmail.com>
2023-04-06 16:12:12 +0430

    q1.py updated

--- q1.py
+++ q1.py
@@ -21,24 +21,43 @@ class User:
     def getUsersList(cls):
         return "\n".join(list(map(str,cls._Users)))
     def __str__(self) -> str:
-        return f"Username = {self.username}/role = {self.role.value}"    
+        return f"Username = {self.username}/role = {self.role.name}"   
+    
+    def save(self):
+        filename = "user.txt" if self.role==Role.OrdinaryUser else "superuser.txt"
+        with open(filename,"a") as f:
+            f.write(str(self)+"\n")
+             
     
 def main():
     # User("moha")
     command = argv[1]
     if command == "createuser":
-        User(argv[2],argv[3],argv[4],argv[5])
+        user = User(argv[2],argv[3],argv[4],argv[5])
+        user.save()
         print("User added successfully")
-    elif command == "CreateSuperuser":
-        User(argv[2],argv[3],argv[4],argv[5],Role.AdminUser)
+        
+        
+        
+    elif command == "Createsuperuser":
+        user = User(argv[2],argv[3],argv[4],argv[5],Role.AdminUser)
+        user.save()
         print("User added successfully")
     elif command == "showList":
         print(User.getUsersList())
     elif command == "Backup":
-        with open("users.txt","a+") as f:
-           f.write(datetime.now())
-           f.write("---------------------------------")
-           f.write(User.getUsersList())
+        with open("user.txt","r") as f:
+            content1 = f.read()
+        with open("superuser.txt","r") as f:
+            content2 = f.read()
+        with open("user-backup.txt","w") as f:
+           f.write(str(datetime.now()))
+           f.write("\n---------------------------------\n")
+           f.write(content1)
+        with open("superuser-backup.txt","w") as f:
+           f.write(str(datetime.now()))
+           f.write("\n---------------------------------\n")
+           f.write(content2)
            
     elif command == "Restore":
           with open(argv[2],"r") as f:      
