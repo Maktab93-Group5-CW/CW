99d16474340c6ef57125270106df9c30f5abb580
Sadegh Shokoohian <Shokoohianm@yahoo.com>
2023-04-06 16:54:50 +0330

    make new codes

---
+++ q5.py
@@ -0,0 +1,11 @@
+import argparse
+parse = argparse.ArgumentParser()
+parse.add_argument('FILE_NAME', type=str)
+parse.add_argument('-n', action='store_true')
+args = parse.parse_args()
+
+with open(args.FILE_NAME, 'r') as f:
+    if args.n:
+        print('Number of lines in the file:', len(f.readlines()))
+    else:
+        print(f.read())
